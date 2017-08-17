af2ff6131418a409d8c0516c2168d45ddaea4426
mecampbellsoup <mecampbell25@gmail.com>
2017-08-17 12:30:57 -0400

    chartjs-node module

--- cryptoChart.js
+++ cryptoChart.js
@@ -9,33 +9,48 @@ var startDate = moment().subtract(1, 'months');
 var startDateUnix = startDate.unix();
 var endDateUnix = endDate.unix();
 
-console.log("Start date:", startDate);
-console.log("End date:", endDate);
-console.log("Start date UNIX:", startDateUnix);
-console.log("End date UNIX:", endDateUnix);
-
 // Fetch the price from that ticker
 var https = require("https");
 ticker = 'ETH';
 var historicalData = {};
 
-for (var m = startDate; m.isBefore(endDate); m.add(1, 'days')) {
+for (var m = startDate; m.isSameOrBefore(endDate); m.add(1, 'days')) {
   mAsUnix = m.unix();
-  getDayPrice(ticker, mAsUnix);
-};
 
-// At this point all historical data should be set in historicalData...
-// But it isn't because this line is reached before the async https.get
-// calls inside of getDayPrice have not finished yet.
-// What to do?
-Promise.resolve(historicalData).then((data) => {
-  console.log("Historical data: ", historicalData);
-});
+  Promise.resolve(getDayPrice(ticker, mAsUnix)).then(function(data) {
+    timestamp = data[0];
+    prices = data[1];
+
+    // m is not changing
+    // but data is
+    console.log("Promise data", data);
+    console.log("m", m);
+    // m moment("2017-08-17T11:40:20.412")
+    //Promise data [ 1502725220, { btc: 0.06919, usd: 299.16 } ]
+    //m moment("2017-08-17T11:40:20.412")
+    //Promise data [ 1500392420, { btc: 0.09854, usd: 227.09 } ]
+    //m moment("2017-08-17T11:40:20.412")
+    //Promise data [ 1501170020, { btc: 0.07626, usd: 202.93 } ]
+    //m moment("2017-08-17T11:40:20.412")
+    //Promise data [ 1502206820, { btc: 0.08662, usd: 296.51 } ]
+    //m moment("2017-08-17T11:40:20.412")
+    //Promise data [ 1502811620, { btc: 0.06897, usd: 286.52 } ]
+    //m moment("2017-08-17T11:40:20.412")
+    //Promise data [ 1501602020, { btc: 0.08285, usd: 225.9 } ]
+    //m moment("2017-08-17T11:40:20.412")
+
+    if (m.isSameOrBefore(endDate)) {
+      console.log(historicalData);
+      var priceHistoryChart = new Chart(ctx, {
+        type: 'line',
+        data: historicalData
+      });
+    };
+  });
+};
 
 //https://min-api.cryptocompare.com/data/pricehistorical?fsym=ETH&tsyms=BTC,USD&ts=1452680400
 function getDayPrice(ticker, timestamp) {
-  var rawData = '';
-
   const options = {
     hostname: 'min-api.cryptocompare.com',
     port: 443,
@@ -45,6 +60,7 @@ function getDayPrice(ticker, timestamp) {
   };
 
   return new Promise(function(resolve, reject) {
+    var rawData = '';
     var parsedJson = '';
 
     https.get(options, (priceResponse) => {
@@ -60,8 +76,7 @@ function getDayPrice(ticker, timestamp) {
           historicalData[timestamp] = {};
           historicalData[timestamp].btc = btcPrice;
           historicalData[timestamp].usd = usdPrice;
-          console.log(historicalData[timestamp]);
-          resolve(historicalData[timestamp]);
+          resolve([timestamp, historicalData[timestamp]]);
         } catch (e) {
           console.error(`Got error: ${e.message}`);
           reject(e);
