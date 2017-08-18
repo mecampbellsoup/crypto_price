bb885fdd7baf9579fc66fc44c59dea19230f8874
mecampbellsoup <mecampbell25@gmail.com>
2017-08-18 12:12:16 -0400

    Resolve all pricePromises in one step

--- cryptoChart.js
+++ cryptoChart.js
@@ -14,40 +14,18 @@ var https = require("https");
 ticker = 'ETH';
 var historicalData = {};
 
+var pricePromises = [];
+
 for (var m = startDate; m.isSameOrBefore(endDate); m.add(1, 'days')) {
   mAsUnix = m.unix();
-
-  Promise.resolve(getDayPrice(ticker, mAsUnix)).then(function(data) {
-    timestamp = data[0];
-    prices = data[1];
-
-    // m is not changing
-    // but data is
-    console.log("Promise data", data);
-    console.log("m", m);
-    // m moment("2017-08-17T11:40:20.412")
-    //Promise data [ 1502725220, { btc: 0.06919, usd: 299.16 } ]
-    //m moment("2017-08-17T11:40:20.412")
-    //Promise data [ 1500392420, { btc: 0.09854, usd: 227.09 } ]
-    //m moment("2017-08-17T11:40:20.412")
-    //Promise data [ 1501170020, { btc: 0.07626, usd: 202.93 } ]
-    //m moment("2017-08-17T11:40:20.412")
-    //Promise data [ 1502206820, { btc: 0.08662, usd: 296.51 } ]
-    //m moment("2017-08-17T11:40:20.412")
-    //Promise data [ 1502811620, { btc: 0.06897, usd: 286.52 } ]
-    //m moment("2017-08-17T11:40:20.412")
-    //Promise data [ 1501602020, { btc: 0.08285, usd: 225.9 } ]
-    //m moment("2017-08-17T11:40:20.412")
-
-    if (m.isSameOrBefore(endDate)) {
-      console.log(historicalData);
-      var priceHistoryChart = new Chart(ctx, {
-        type: 'line',
-        data: historicalData
-      });
-    };
-  });
+  console.log("Pushing getDayPrice for " + mAsUnix + " into pricePromises...");
+  pricePromises.push(getDayPrice(ticker, mAsUnix));
 };
+
+Promise.resolve(pricePromises).then(function(data) {
+  console.log("Resolving price promises...");
+  console.log(data);
+});
 
 //https://min-api.cryptocompare.com/data/pricehistorical?fsym=ETH&tsyms=BTC,USD&ts=1452680400
 function getDayPrice(ticker, timestamp) {
