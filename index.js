7ed16a23d1dc7b9aed031cda9f636e8e8ce9d2b3
mecampbellsoup <mecampbell25@gmail.com>
2017-08-21 18:27:53 -0400

    Put all JS in index.js

--- index.js
+++ index.js
@@ -5,33 +5,112 @@
  * @param {Object} res Cloud Function response context.
  */
 
-var yaml = require('js-yaml');
-var fs = require('fs');
+const yaml = require('js-yaml');
+const fs = require('fs');
 const tickersMap = yaml.load(fs.readFileSync('dictionary.yml'));
+const moment = require('moment');
 
+//https://min-api.cryptocompare.com/data/pricehistorical?fsym=ETH&tsyms=BTC,USD&ts=1452680400
+function getDayPrice(ticker, timestamp) {
+  const options = {
+    hostname: 'min-api.cryptocompare.com',
+    port: 443,
+    path: '/data/pricehistorical?fsym=' + ticker + '&tsyms=BTC,USD&ts=' + timestamp,
+    method: 'GET',
+    headers: { 'Accept': 'application/json' }
+  };
+
+  return new Promise(function(resolve, reject) {
+    var rawData = '';
+    var parsedJson = '';
+
+    https.get(options, (priceResponse) => {
+      priceResponse.on('data', (d) => {
+        rawData += d;
+      });
+
+      priceResponse.on('end', () => {
+        try {
+          parsedJson = JSON.parse(rawData)[ticker];
+          btcPrice = parsedJson['BTC'];
+          usdPrice = parsedJson['USD'];
+          historicalData[timestamp] = {};
+          historicalData[timestamp].btc = btcPrice;
+          historicalData[timestamp].usd = usdPrice;
+          resolve([timestamp, historicalData[timestamp]]);
+        } catch (e) {
+          console.error(`Got error: ${e.message}`);
+          reject(e);
+        }
+      });
+    }).on('error', (e) => {
+      console.error(`Got error: ${e.message}`);
+    });
+  })
+};
+
+//////////////////////
+// fetchCryptoChart //
+//////////////////////
+//
 exports.fetchCryptoChart = function fetchCryptoChart (req, res) {
   // Log request details
   if (req.body.text === undefined) {
     // This is an error case, as "text" is required
     res.status(400).send('No text defined!');
   } else {
-    //1. Set the ticker from params['text']
-    //2. Get the 1-month historical price data (daily data points)
-    //3. Render a chart as an image file
-    //4. Respond with that image to Slack command
-    var chartJson = {
-      "text": "Bitcoin price chart... or doge?",
-      "attachments": [
-        {
-          "image_url": "http://bitcoinmacroeconomics.com/wp-content/uploads/2014/11/doge1115.png"
-        }
-      ]
+    var endDate = moment();
+    var startDate = moment().subtract(1, 'months');
+
+    var startDateUnix = startDate.unix();
+    var endDateUnix = endDate.unix();
+
+    // Fetch the price from that ticker
+    var https = require("https");
+    ticker = 'ETH';
+    var historicalData = {};
+
+    var pricePromises = [];
+
+    for (var m = startDate; m.isSameOrBefore(endDate); m.add(1, 'days')) {
+      mAsUnix = m.unix();
+      console.log("Pushing getDayPrice for " + mAsUnix + " into pricePromises...");
+      pricePromises.push(getDayPrice(ticker, mAsUnix));
+    };
+
+    Promise.all(pricePromises).then(function(data) {
+      var timestamps = [];
+      var btcPrices = [];
+      var usdPrices = [];
+
+      data.forEach(function(datumWithTimestamp) {
+        timestamps.push(datumWithTimestamp[0]);
+        btcPrices.push(datumWithTimestamp[1].btc);
+        usdPrices.push(datumWithTimestamp[1].usd);
+      });
+
+      var exec = require("child_process").exec;
+      var result = exec('ruby -e "puts \'Hello from Ruby!\'"', function (err, stdout, stderr) {
+        console.log(stdout);
+      });
+
+      var chartJson = {
+        "text": result,
+        "attachments": [
+          {
+            "image_url": "http://bitcoinmacroeconomics.com/wp-content/uploads/2014/11/doge1115.png"
+          }
+        ]
+      }
+
+      res.status(200).json(chartJson);
     }
+  };
 
-    res.status(200).json(chartJson);
-  }
-};
-
+//////////////////////
+// fetchCryptoPrice //
+//////////////////////
+//
 exports.fetchCryptoPrice = function fetchCryptoPrice (req, res) {
   // Log request details
   if (req.body.text === undefined) {
